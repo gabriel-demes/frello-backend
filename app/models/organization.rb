081a5ff7e850b5196cda3a14c14722ba9150457f
gabriel-demes <Gabrieldemes@nyu.edu>
2021-03-10 19:08:25 -0500

    finally frontend backedn sync

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,8 @@
 class Organization < ApplicationRecord
     has_many :lists
     has_many :taskcards, through: :lists
-    has_many :memberships
-    has_many :users, through: :memberships 
+    has_many :memeberships
+    has_many :users, through: :memeberships 
 
     
        
